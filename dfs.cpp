62834d03cac4b1f3d8bc8fd9742e9c3405cca5e1
neelrathod46 <neelrathod4678@gmail.com>
2026-04-05 19:49:44 +0530

    Depth-First Search in C++

--- dfs.cpp
+++ dfs.cpp
@@ -2,15 +2,25 @@
 #include <vector>
 using namespace std;
 
-class graph {
-    private:
-    vector<vector<int>> adjList = {{}, {2,6}, {1,3,4}, {2}, {2,5}, {4,8}, {1,7,9}, {6,8,9}, {5,7}, {6}};
-    int visited[10] = 0;
-    public:
-    void dfs(int node)
-}
-void dfs(int node, vector<vector<int>> &adjList);
+void dfs(int node, vector<vector<int>> &adjList, vector<int> &visited);
+
 int main() {
-
+  vector<vector<int>> adjList = {{}, {2,6}, {1,3,4}, {2}, {2,5}, {4,8}, {1,7,9}, {6,8,9}, {5,7}, {6}};
+  vector<int> visited(10, 0);
+  dfs(1, adjList, visited);
+  for (int n: visited) cout << n << " ";
+  cout << "\n";
 }
 
+void dfs(int node, vector<vector<int>> &adjList, vector<int> &visited) {
+  for (int neighbour: adjList[node]) {
+    if(!visited[neighbour]) {
+      visited[neighbour] = 1;
+      cout << "Node: " << neighbour << " visited: ";
+      for (int n: visited) cout << n << " ";
+      cout << "\n";
+      dfs(neighbour, adjList, visited);
+    }
+
+  }
+}
